e43b4a156a1a806b1d1e6f92f7b094e6bfaf0c3e
bygimenez <davidalbal123@gmail.com>
2023-07-07 02:16:48 +0200

    transition update

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,10 +1,25 @@
-function spaNavigate(data) {
-	// Fallback for browsers that don't support this API:
-	if (!document.startViewTransition) {
-	  updateTheDOMSomehow(data);
-	  return;
-	}
+import { getPageContent, onLinkNavigate } from '../utils.js';
+
+onLinkNavigate(async ({ toPath }) => {
+  const content = await getPageContent(toPath);
   
-	// With a transition:
-	document.startViewTransition(() => updateTheDOMSomehow(data));
+  startViewTransition(() => {
+    // This is a pretty heavy-handed way to update page content.
+    // In production, you'd likely be modifying DOM elements directly,
+    // or using a framework.
+    // innerHTML is used here just to keep the DOM update super simple.
+    document.body.innerHTML = content;  
+  });
+});
+
+
+// A little helper function like this is really handy
+// to handle progressive enhancement.
+function startViewTransition(callback) {
+  if (!document.startViewTransition) {
+    callback();
+    return;
   }
+  
+  document.startViewTransition(callback);
+}
