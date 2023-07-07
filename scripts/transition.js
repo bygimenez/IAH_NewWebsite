84dc75b2aa79216767635be71235199f7df121a6
bygimenez <davidalbal123@gmail.com>
2023-07-07 16:58:55 +0200

    fix

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,28 +1,28 @@
 // try to fix later!
 
 
-import { getPageContent, onLinkNavigate } from '../scripts/utils.js';
+// import { getPageContent, onLinkNavigate } from '../scripts/utils.js';
 
-onLinkNavigate(async ({ toPath }) => {
-  const content = await getPageContent(toPath);
+// onLinkNavigate(async ({ toPath }) => {
+//   const content = await getPageContent(toPath);
   
-  startViewTransition(() => {
-    // This is a pretty heavy-handed way to update page content.
-    // In production, you'd likely be modifying DOM elements directly,
-    // or using a framework.
-    // innerHTML is used here just to keep the DOM update super simple.
-    document.body.innerHTML = content;  
-  });
-});
+//   startViewTransition(() => {
+//     // This is a pretty heavy-handed way to update page content.
+//     // In production, you'd likely be modifying DOM elements directly,
+//     // or using a framework.
+//     // innerHTML is used here just to keep the DOM update super simple.
+//     document.body.innerHTML = content;  
+//   });
+// });
 
 
-// A little helper function like this is really handy
-// to handle progressive enhancement.
-function startViewTransition(callback) {
-  if (!document.startViewTransition) {
-    callback();
-    return;
-  }
+// // A little helper function like this is really handy
+// // to handle progressive enhancement.
+// function startViewTransition(callback) {
+//   if (!document.startViewTransition) {
+//     callback();
+//     return;
+//   }
   
-  document.startViewTransition(callback);
-}
+//   document.startViewTransition(callback);
+// }
